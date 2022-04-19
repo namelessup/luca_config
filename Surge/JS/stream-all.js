9bbad1ce2164ea6485b85caa36421926e71f3389
namelessup <fumin.chen@outlook.com>
2022-04-19 23:59:00 +0800

    Update stream-all.js

--- Surge/JS/stream-all.js
+++ Surge/JS/stream-all.js
@@ -23,7 +23,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       title: 'Streaming Unlock Detection',
       content: '',
       icon: 'play.circle.fill',
-      'icon-color': '#43CD80',
+      'icon-color': '#CD950C',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
